5c5e2c5c64507749e31497c8d37333fd816f3a77
Peter Hashem <peterhashem@gmail.com>
2023-08-30 18:33:27 -0400

    added comments

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,6 +6,12 @@ const assertEqual = function (actual, expected) {
   }
 };
 
+/**
+ * It scans the object to find the first key that corresponds to the given value.
+ * @param {object} object object taken it to find its key
+ * @param {string} value the value of the key we are looking for
+ * @returns undefined if no key is found within the object
+ */
 const findKeyByValue = function (object, value) {
   for (const key in object) {
     if (object[key] === value) {
