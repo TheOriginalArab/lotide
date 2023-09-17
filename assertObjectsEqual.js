f44d550ecd3b1793450fbc53beec210f71a05c60
Peter Hashem <peterhashem@gmail.com>
2023-09-16 21:42:45 -0400

    refactored code

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,23 +1,4 @@
-const eqObjects = function (object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-
-  for (const key of keys1) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
-    } else if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+const eqObjects = require("./eqObjects");
 
 const assertObjectsEqual = function (actual, expected) {
   const inspect = require("util").inspect;
@@ -31,3 +12,5 @@ const assertObjectsEqual = function (actual, expected) {
     );
   }
 };
+
+module.exports = assertObjectsEqual;
